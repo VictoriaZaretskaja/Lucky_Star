4ac79a8c7bf91cd3f3051d5a335dc9efc4b917b5
Victoria <victoria.zaretskaja@gmail.com>
2016-07-20 21:05:14 +0300

    122

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+inport requests
 
 app = Flask(__name__)
 
@@ -8,6 +9,12 @@ def test():
 
 @app.route("/hook", methods=['POST'])
 def hook():
+    chat_id = request.get_json()["message"]["chat"]["id"]
+    requests.post("https://api.telegram.org/bot253643907:AAGRFi8w-YJiyHw-2qioYIn2wQJMdLx-cnQ/sendMessage",
+        {
+            "chat_id": chat_id,
+            "text": "Hi!"
+        })
     return "OK"
 
 
