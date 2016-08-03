ee6005994e56fe199858ba11ee9a2fb32ff67280
Victoria <victoria.zaretskaja@gmail.com>
2016-08-03 20:14:09 +0300

    12

--- bot.py
+++ bot.py
@@ -1,21 +1,45 @@
 from flask import Flask, request
 import requests
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+client = MongoClient("mongodb://<Victoria>:<260784zvg>@ds029635.mlab.com:29635/heroku_3gwq73vd")
+db = client.heroku_3gwq73vd
+
 
 @app.route('/')
 def test():
     return "It works!"
 
+def send():
+    requests.post("https://api.telegram.org/bot253643907:AAGRFi8w-YJiyHw-2qioYIn2wQJMdLx-cnQ/sendMessage",
+                  {
+                      "chat_id": chat_id,
+                      "text": "Hi!"
+                  })
+
 @app.route("/hook", methods=['POST'])
 def hook():
     chat_id = request.get_json()["message"]["chat"]["id"]
-    requests.post("https://api.telegram.org/bot253643907:AAGRFi8w-YJiyHw-2qioYIn2wQJMdLx-cnQ/sendMessage",
-        {
-            "chat_id": chat_id,
-            "text": "Hi!"
-        })
+    text = request.get_json() ["message"] ["text"]
+
+    command, *args = text.split()
+
+    if command == "/add":
+        db.products.insert({"products": args})
+        send(chat_id, "Products added")
+
+    if command =="/get":
+        answer = "\n".join(
+            map(str, db.products.find())
+        )
+        send(chat_id, answer)
+
+
     return "OK"
+
+
 
 
 
