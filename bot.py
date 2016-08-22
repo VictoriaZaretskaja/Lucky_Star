01a042e4a7278ab49e64e927ddb68eb14a23f888
Victoria <victoria.zaretskaja@gmail.com>
2016-08-23 01:18:28 +0300

    2

--- bot.py
+++ bot.py
@@ -37,9 +37,7 @@ def hook():
         answer = random.choice(db.products.find())
         send(chat_id, answer)
     if command == "/Help":
-        answer = "\n".join(
-            map(str, print("You could add a dish with command /add_dish", "and give a dish with command /dish"))
-        )
+        answer = "You could add a dish with command /add_dish", "and give a dish with command /dish"
         send(chat_id, answer)
     if command == "/Hi":
         answer = "Hi, Lucky's friend!"
