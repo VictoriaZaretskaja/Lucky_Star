a608daaad924d62b768ec85b2e895e3f351e80de
Victoria <victoria.zaretskaja@gmail.com>
2016-08-29 00:53:59 +0300

    new185551

--- bot.py
+++ bot.py
@@ -20,6 +20,22 @@ def send(chat_id, text):
                   })
 
 
+def video(bot, update, msg):
+    link = urllib.parse.urlencode({"search_query": msg})
+    content = urllib.request.urlopen("https://www.youtube.com/results?" + link)
+    search_results = re.findall('href=\"\/watch\?v=(.*?)\"', content.read().decode())
+    if len(search_results) > 0:
+        # Первые 10 результатов
+        search_results = search_results[0:9:1]
+        choice_f = random_number.choice(search_results)
+        yt_link = "https://www.youtube.com/watch?v=" + choice_f
+        bot.sendMessage(update.message.chat_id, text=yt_link, parse_mode=ParseMode.MARKDOWN)
+    else:
+        bot.sendMessage(update.message.chat_id, text='Ничего не найдено.')
+
+products = ["Салат 'Цезарь' Ингредиенты: Кочан пекинской капусты, хлеб серый смешанный(украинский) 3-4 ломтика",
+            'Masha', 'Lena', 'Oleg', 'Dima']
+
 @app.route("/hook", methods=['POST'])
 def hook():
     chat_id = request.get_json()["message"]["chat"]["id"]
@@ -31,27 +47,17 @@ def hook():
     if command == "/Hi":
         answer = "Hi, LUCKY`s Friend!"
         send(chat_id, answer)
+    if command == "/dish1":
+        answer = random.choice(products)
+        send(chat_id, answer)
     if command == "/pic":
         answer = "http://www.virtusinterpress.org/IMG/jpg/ukr_26740.jpg"
         send(chat_id, answer)
     if command == "video":
-        def video(bot, update, msg):
-            link = urllib.parse.urlencode({"search_query": msg})
-            content = urllib.request.urlopen("https://www.youtube.com/results?" + link)
-            search_results = re.findall('href=\"\/watch\?v=(.*?)\"', content.read().decode())
-            if len(search_results) > 0:
-                # Первые 10 результатов
-                search_results = search_results[0:9:1]
-                choice_f = random_number.choice(search_results)
-                yt_link = "https://www.youtube.com/watch?v=" + choice_f
-                bot.sendMessage(update.message.chat_id, text=yt_link, parse_mode=ParseMode.MARKDOWN)
-            else:
-                bot.sendMessage(update.message.chat_id, text='Ничего не найдено.')
+
         send(chat_id, video())
     if command == "/dish":
-        answer = "\n".join(
-            map(str, random.choice(db.products))
-        )
+        answer = random.choice(db.products)
         send(chat_id, answer)
 
     return "OK"
