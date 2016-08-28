ec199e8f66020ea14254207f3c450a84cc3702f1
Victoria <victoria.zaretskaja@gmail.com>
2016-08-28 21:15:26 +0300

    new11

--- bot.py
+++ bot.py
@@ -1,6 +1,15 @@
 from flask import Flask, request
 import requests
 from pymongo import MongoClient
+from telegram import Updater, Emoji, ParseMode
+import telegram
+from time import sleep
+import logging
+import requests, json
+import urllib.request, urllib.parse,urllib
+import urllib.request
+import re, sys, os, platform
+import random  as  random_number
 import random
 
 app = Flask(__name__)
@@ -29,9 +38,25 @@ def hook():
     if command == "/Hi":
         answer = "Hi, LUCKY`s Friend!"
         send(chat_id, answer)
-    if command == "/Help":
+    if command == "/help":
         answer = "You could add a dish with command /add_dish or give a dish with"
         send(chat_id, answer)
+    if command == "/1":
+        answer = "It's a very crazy bot)"
+        send(chat_id, answer)
+
+    def video(bot, update, msg):
+        link = urllib.parse.urlencode({"search_query": msg})
+        content = urllib.request.urlopen("https://www.youtube.com/results?" + link)
+        search_results = re.findall('href=\"\/watch\?v=(.*?)\"', content.read().decode())
+        if len(search_results) > 0:
+            # Первые 10 результатов
+            search_results = search_results[0:9:1]
+            choice_f = random_number.choice(search_results)
+            yt_link = "https://www.youtube.com/watch?v=" + choice_f
+            bot.sendMessage(update.message.chat_id, text=yt_link, parse_mode=ParseMode.MARKDOWN)
+        else:
+            bot.sendMessage(update.message.chat_id, text='Ничего не найдено.')
     if command == "/list":
         answer = "\n".join(
             map(str, db.products.find({}))
