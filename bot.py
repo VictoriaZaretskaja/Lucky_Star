512e13ff7692641872badf37b5721a425cff8ce8
Victoria <victoria.zaretskaja@gmail.com>
2016-08-29 23:57:46 +0300

    new187751

--- bot.py
+++ bot.py
@@ -33,12 +33,8 @@ def video(bot, update, msg):
     else:
         bot.sendMessage(update.message.chat_id, text='Ничего не найдено.')
 
-dishes = ['Салат Ингредиенты: Кочан пекинской капусты, хлеб серый смешанный(украинский) 3-4 ломтика',
+products = ['Салат Ингредиенты: Кочан пекинской капусты, хлеб серый смешанный(украинский) 3-4 ломтика',
             'Masha', 'Lena', 'Oleg', 'Dima']
-
-
-meal = ['Гречка и фрикассе', 'Макароны с соусом Болоньез', 'Отварной картофель с колбасками',
-        'Картофель пюре с котлетами', 'Рис с рыбными котлетами' ]
 
 @app.route("/hook", methods=['POST'])
 def hook():
@@ -48,17 +44,11 @@ def hook():
     if command == "/add_dish":
         db.products.insert({"dish": args})
         send(chat_id, "Dish added")
-    if command == "/hi":
+    if command == "/Hi":
         answer = "Hi, LUCKY`s Friend!"
         send(chat_id, answer)
-    if command == "/dish":
-        answer = random.choice(dishes)
-        send(chat_id, answer)
-    if command == "/meal":
-        answer = random.choice(meal)
-        send(chat_id, answer)
-    if command == "/help":
-        answer = "command /dish - интересное блюдо, command /meal - блюдо на каждый день"
+    if command == "/dish1":
+        answer = random.choice(products)
         send(chat_id, answer)
     if command == "/pic":
         answer = "http://www.virtusinterpress.org/IMG/jpg/ukr_26740.jpg"
@@ -66,5 +56,8 @@ def hook():
     if command == "video":
 
         send(chat_id, video())
+    if command == "/dish":
+        answer = random.choice(db.products)
+        send(chat_id, answer)
 
     return "OK"
